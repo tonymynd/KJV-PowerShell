9d9fd2b65d9cf6c42a57d26d06ee4c88bcc1a013
tonymynd <84558942+tonymynd@users.noreply.github.com>
2024-09-14 14:36:52 -0600

    Update bible.psm1
    Pasted code

--- bible/bible.psm1
+++ bible/bible.psm1
@@ -1,1 +1,26 @@
+function Read-Bible {
+    [CmdletBinding()]
+    param (
+        [string]$Book = "John",
+        [int]$Chapter = 3,
+        [int]$Verse = 16
+    )
+
+    $bible = @{
+        "John" = @{
+            3 = @{
+                16 = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."
+            }
+        }
+        # Add more books, chapters, and verses here as needed.
+    }
+
+    if ($bible.ContainsKey($Book) -and $bible[$Book].ContainsKey($Chapter) -and $bible[$Book][$Chapter].ContainsKey($Verse)) {
+        return $bible[$Book][$Chapter][$Verse]
+    } else {
+        Write-Error "Verse not found."
+    }
+}
+
+Export-ModuleMember -Function Read-Bible
 
